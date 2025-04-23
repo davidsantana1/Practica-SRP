9900181f0c56a63d8871d8c93af73d8728cfc0da
davidsantana1 <davidsantanar01@gmail.com>
2025-04-23 16:56:29 -0400

    Add project files.

---
+++ Practica SRP/Persona.cs
@@ -0,0 +1,29 @@
+﻿namespace Practica_SRP
+{
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+        public string direccion;
+        public string correoElectronico;
+    }
+
+    public class Correos
+    {
+        public void EnviarCorreoElectronico(string mensaje)
+        {
+            // Lógica para enviar correo electrónico
+        }
+    }
+
+    public class ImprimirPersona
+    {
+        public void ImprimirDatos(Persona persona)
+        {
+            Console.WriteLine($"Nombre: {persona.nombre}");
+            Console.WriteLine($"Edad: {persona.edad}");
+            Console.WriteLine($"Dirección: {persona.direccion}");
+            Console.WriteLine($"Correo electrónico: {persona.correoElectronico}");
+        }
+    }
+}
